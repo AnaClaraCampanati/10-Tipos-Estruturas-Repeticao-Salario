a8596f577fb2e08bde27df5d171d30e415d6eceb
Ana Clara campanati <campanatianaclara@gmail.com>
2023-01-09 13:24:42 -0300

    Extraído a regra de calculo de reajuste para classe Salário.

--- 10-Tipos-Estruturas-Repeticao-Salario/Program.cs
+++ 10-Tipos-Estruturas-Repeticao-Salario/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Faça um algoritmo que calcula e exiba o salário reajustado de 10 funcionários de acordo com
 // a seguinte regra: Salário até 300, reajuste de 50%; Salários maiores que 300, reajuste de 30%.
 
+using _10_Tipos_Estruturas_Repeticao_Salario;
+
 double novoSalario = 0;
 
 List<double> salarios = new List<double>();
@@ -11,13 +13,7 @@ for (int i = 0; i < 10; i++)
     Console.WriteLine("Digite o sálario:");
     double salario = Convert.ToDouble(Console.ReadLine());
 
-    salarios.Add(salario);
-
-    if (salario <= 300)
-        novoSalario = salario * 0.5 + salario;
-    if (salario > 300)
-        novoSalario = salario * 0.3 + salario;
-
+    novoSalario = Salario.CalculaReajuste(salario);
 
     Console.WriteLine("Salário reajustado: " + novoSalario);
 }
---
+++ 10-Tipos-Estruturas-Repeticao-Salario/Salario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Tipos_Estruturas_Repeticao_Salario
+{
+    public static class Salario
+    {
+        public static double CalculaReajuste(double salario)
+        {
+            if (salario <= 300)
+                return salario * 0.5 + salario;
+            if (salario > 300)
+                return salario * 0.3 + salario;
+
+            return salario;
+        }
+    }
+}
